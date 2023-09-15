136d9e591a7306ea2c346ba65eddbd05ae0fa08b
Airblow <143172177+Airblow@users.noreply.github.com>
2023-09-15 12:26:54 +0200

    Add project files.

---
+++ Inlämning2.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace inlämning2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din favoritbok?");
+            string bok = Console.ReadLine();
+            Console.WriteLine("Din favoritbok är alltså " + bok);
+            Console.ReadKey();
+        }
+
+    }
+
+
+
+}
